b794acef26c471ea9aaac8e6f08cd095f6a74793
davidcoats <david@getsafetycone.com>
2023-10-30 16:00:01 -0700

    Initial commit.

---
+++ source/R5T.L0069/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.L0069
+{
+	/// <summary>
+	/// Documentation target types library.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.L0069/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.L0069
+Documentation target types library.
